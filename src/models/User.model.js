ddba2b94421c130ad3bda1bfec70a64978e3ef99
stefanbinoj <stefan.binoj.007@gmail.com>
2024-10-18 19:56:46 +0530

    cloudinary

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -52,4 +52,23 @@ userSchema.methods.isPasswordCorrect = async function (password) {
     
 }
 
+userSchema.methods.accessTokenGen = function(){
+    return jwt.sign({
+        _id:this._id,
+        username:this.username,
+        email:this.email
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {expiresIn:process.env.ACCESS_TOKEN_EXPIRY}
+)
+}
+userSchema.methods.refreshTokenGen = function(){
+    return jwt.sign({
+        _id:this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {expiresIn:process.env.REFRESH_TOKEN_EXPIRY}
+)
+}
+
 export const User =  mongoose.model("User",userSchema)
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,24 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs';
+// Configuration
+cloudinary.config({ 
+    cloud_name:process.env.CLOUDINARY_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY , 
+    api_secret:process.env.CLOUDINARY_API_SECRET  // Click 'View API Keys' above to copy your API secret
+});
+
+const uploadToCloudinary = async(filePath) =>{
+    try {
+        if(!filePath)return null
+        const uploadResult = await cloudinary.uploader.upload(filePath, {
+            resource_type:'auto'}
+        )
+        console.log("File Uploaded Successfully : ",uploadResult.url)
+        return uploadResult
+    } catch (error) {
+        fs.unlinkSync(filePath)
+        console.log("The Error is : ",error)
+        return null
+    }
+}
+export {uploadToCloudinary}
