048ea3b45420b4c6911fc928902f765a4126007d
stefanbinoj <stefan.binoj.007@gmail.com>
2024-10-18 19:00:26 +0530

    secure tokens

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -41,4 +41,15 @@ const userSchema = new mongoose.Schema({
     }
 },{timestamps:true})
 
+userSchema.pre('save',async function(next){
+    if(!this.isModified("password")) return next()
+    this.password=bcrypt.hash(this.password,10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password,this.password)
+    
+}
+
 export const User =  mongoose.model("User",userSchema)
